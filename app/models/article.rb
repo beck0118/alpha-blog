93ba2b26815c57599510da850f778e43eccfcd71
Keijiro <k2jxxx@gmail.com>
2019-05-11 18:03:34 +0900

    change validate

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-    validates :title, presence: true, length:{minimum: 3, maximum: 60}
+    validates :title, presence: true, length:{minimum: 2, maximum: 60}
     validates :description, presence: true, length:{minimum:5 , maximum:200}
 
 end
