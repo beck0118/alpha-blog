85e6f4d1aa92082265c21c406296c680f153f671
Keijiro <k2jxxx@gmail.com>
2019-06-05 23:37:24 +0900

    debugger

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,9 @@ def edit
 end
 
 def create
+    debugger
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
         flash[:success]="Your Heart will lead the world!!"
         redirect_to article_path(@article)
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
+    belongs_to :user
     validates :title, presence: true, length:{minimum: 2, maximum: 60}
     validates :description, presence: true, length:{minimum:5 , maximum:200}
+    validates :user_id, presence: true
 
 end
