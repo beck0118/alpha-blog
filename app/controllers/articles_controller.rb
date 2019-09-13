fa1b2d071a335e25991c5236ee33348b928fcbce
Keijiro <k2jxxx@gmail.com>
2019-09-13 15:45:35 +0900

    add admin user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -53,7 +53,7 @@ private
     end
 
     def require_same_user
-      if current_user != @article.user
+      if current_user != @article.user and !current_user.admin?
         flash[:danger] = "編集権限がありません"
         redirect_to root_path
       end
